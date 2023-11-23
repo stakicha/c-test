2b32808e2c153c22122184d97ac7c8d04fe0ec1e
СтаниславДГеоргиев <@LAPTOP-K14GEF6I>
2023-11-23 11:17:55 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Add(2, 2);
+    }
+
+    public static int Add(int x, int y)
+    {
+        int c = x + y;
+        return c;
+    }
+}
